c71c99047e8d06d03b85a4f8a210ad4dac65664c
Fengyarnom <fengyarnom@gmail.com>
2022-01-08 19:45:08 +0800

    :heavy_plus_sign: Add `react-router-dom` dependency This commit added `react-router-dowm`.In fact,i faced some problems,the biggest problem is that the router-dom version is too high(ps.version 6.x),so the `Switch` component has been replaced by `Routers` component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from "./components/header";
 import Home from "./pages/Home"
 export default class APP extends React.Component{
   render(){
     return(
       <div className="App">
-        <Header></Header>
-        <Home></Home>
+        <Router>
+          <Header></Header>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            
+          </Routes>
+        </Router>
       </div>
     )
   }
