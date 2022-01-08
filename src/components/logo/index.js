0d32692b4bc992ca9a6142a1e15ba774ecd74f74
Fengyarnom <fengyarnom@gmail.com>
2022-01-08 11:36:59 +0800

    :sparkles: Adapted to the header module on the mobile phone website

--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -3,11 +3,19 @@ export default class Logo extends React.Component{
   render(){
     return(
       <div className="logo">
-        <div className="logo-text"> 
+        <div className="logo-text">
+
                 <span className="tag1">Yarnom</span>
                 <span className="tag2">-</span>
-                <span className="tag3">Blog</span>        
-            </div>
+                <span className="tag3">Blog</span>   
+                    
+        </div>
+        <div className="menuIcon">
+          <a href="">
+            <svg t="1641610771983" className="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2309" width="200" height="200"><path d="M133.31 296.552h757.207c19.782 0 35.951-16.169 35.951-35.95 0-19.782-15.997-35.952-35.95-35.952H133.31c-19.782 0-35.951 16.17-35.951 35.951 0 19.782 16.17 35.951 35.95 35.951zM890.517 476.135H133.311c-19.782 0-35.951 16.17-35.951 35.951 0 19.782 16.17 35.951 35.95 35.951h757.207c19.782 0 35.951-16.17 35.951-35.951 0-19.782-16.17-35.95-35.95-35.95zM890.517 727.448H133.311c-19.782 0-35.951 15.997-35.951 35.95s16.17 35.952 35.95 35.952h757.207c19.782 0 35.951-15.998 35.951-35.951s-16.17-35.951-35.95-35.951z" p-id="2310" fill="#515151"></path></svg>
+          </a>
+          
+        </div>
       </div>
     )
   }
--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -6,6 +6,7 @@ export default class NavBar extends React.Component{
   render(){
     return(
       <div className="navBar">
+    
         <Logo></Logo>
         <NavList></NavList>
         <Options></Options>
